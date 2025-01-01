832888672ae5cffadad86ba1c36ca60e7d7a7e3e
Luke Giuliani <luke@giuliani.com.au>
2025-01-01 22:59:22 +1100

    move to github pages for site (A and AAAA records), and also dkim is fun

--- iac/main.tf
+++ iac/main.tf
@@ -42,21 +42,85 @@ data "cloudflare_zones" "domain" {
   }
 }
 
-# Web
-resource "cloudflare_record" "www-root" {
+# Web - github pages
+resource "cloudflare_record" "www-root-1" {
   zone_id = data.cloudflare_zones.domain.zones[0].id
   name    = "@"
-  content = "103.209.24.16"
+  content = "185.199.108.153"
   type    = "A"
   proxied = true
   ttl     = 1 # Auto TTL
 }
 
-resource "cloudflare_record" "www" {
+resource "cloudflare_record" "www-root-2" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "@"
+  content = "185.199.109.153"
+  type    = "A"
+  proxied = true
+  ttl     = 1 # Auto TTL
+}
+
+resource "cloudflare_record" "www-root-3" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "@"
+  content = "185.199.110.153"
+  type    = "A"
+  proxied = true
+  ttl     = 1 # Auto TTL
+}
+
+resource "cloudflare_record" "www-root-4" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "@"
+  content = "185.199.111.153"
+  type    = "A"
+  proxied = true
+  ttl     = 1 # Auto TTL
+}
+
+# Web - github pages
+resource "cloudflare_record" "www6-root-1" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "@"
+  content = "2606:50c0:8000::153"
+  type    = "AAAA"
+  proxied = true
+  ttl     = 1 # Auto TTL
+}
+
+resource "cloudflare_record" "www6-root-2" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "@"
+  content = "2606:50c0:8001::153"
+  type    = "AAAA"
+  proxied = true
+  ttl     = 1 # Auto TTL
+}
+
+resource "cloudflare_record" "www6-root-3" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "@"
+  content = "2606:50c0:8002::153"
+  type    = "AAAA"
+  proxied = true
+  ttl     = 1 # Auto TTL
+}
+
+resource "cloudflare_record" "www6-root-4" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "@"
+  content = "2606:50c0:8003::153"
+  type    = "AAAA"
+  proxied = true
+  ttl     = 1 # Auto TTL
+}
+
+resource "cloudflare_record" "www-cname" {
   zone_id = data.cloudflare_zones.domain.zones[0].id
   name    = "www"
-  content = "103.209.24.16"
-  type    = "A"
+  content = "@"
+  type    = "CNAME"
   proxied = true
   ttl     = 1 # Auto TTL
 }
@@ -130,6 +194,14 @@ resource "cloudflare_record" "dmarc" {
   ttl     = 3600
 }
 
+resource "cloudflare_record" "dkim" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "google._domainkey"
+  content = "\"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvTjRMA5jN6+xOCsXoROFLXYNFbqMyN5q9FaQyn9YVrMPOs99QsHddXiFcQ0DcZESO9YYpUoxoqr41iJk4XsEcOmnM2hB181IfA+cnYjMcvSisY2DqlFjPzQuRL9T0x4Yx9BU0kepxgLdnj8lSUeipmYd8XTiYG/oaMJU7FiMkSqpzZdJ9ng/oEhgvdS7ln/1bHJYDVxMOYpaETQYw5izYdaMDM5GqAB+w7vHlBXUmBLakEdiCIGlKQGXQQR3SVwntXTQ/jBwQftMGyF4ys0wecuvjKvq5fhiaWYavQWW9Mp+6nNJ6l1b443G44lLWP0shUfsUlHQPx8Z4dYOva7WxQIDAQAB\""
+  type    = "TXT"
+  proxied = false
+  ttl     = 3600
+}
 
 resource "cloudflare_record" "cname_calendar" {
   zone_id = data.cloudflare_zones.domain.zones[0].id
